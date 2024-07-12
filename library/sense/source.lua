3a66e906c8a2a8e2def4c9105095bb3bea76e414
de_Neuublue <88302612+Neuublue@users.noreply.github.com>
2024-07-12 12:26:20 -0400

    god it's so retarded

--- library/sense/source.lua
+++ library/sense/source.lua
@@ -212,13 +212,9 @@ function EspObject:Update()
 
 	local _, onScreen, depth = worldToScreen(head.Position);
 	self.onScreen = onScreen;
+	self.distance = depth;
 
-    self.rootPart = findFirstChild(self.character, "HumanoidRootPart");
-    local distance = (self.rootPart.Position - head.Position).Magnitude;
-
-	self.distance = distance;
-
-	if interface.sharedSettings.limitDistance and distance > interface.sharedSettings.maxDistance then
+	if interface.sharedSettings.limitDistance and depth > interface.sharedSettings.maxDistance then
 		self.onScreen = false;
 	end
 
@@ -517,8 +513,7 @@ function InstanceObject:Render()
 	local world = getPivot(instance).Position;
 	local position, visible, depth = worldToScreen(world);
 
-    self.rootPart = findFirstChild(self.character, "HumanoidRootPart");
-    local distance = (self.rootPart.Position - world).Magnitude;
+    local distance = (world - self.interface:getCharacter(localPlayer):FindFirstChild('HumanoidRootPart').Position).Magnitude
 
 	if options.limitDistance and distance > options.maxDistance then
 		visible = false;
